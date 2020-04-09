160721b16344352ed2abf3890a7f7f4948422f10
Infernio <infernio@icloud.com>
2020-04-09 19:14:09 +0200

    FO3, FNV, TES5: Port Tweak Assorted
    Features everything valda's version features plus four more tweaks that
    could be easily ported (tested and confirmed working).
    
    I renamed the 'potion weight minimum' tweak to both be more accurate (it
    doesn't just target potions, but all ingestibles - including drinks) and
    so that it makes sense for FO3/FNV.
    
    Note I completely rewrote the 'playable skips' algorithm to minimize
    regex usage, turns out that regex was *super* slow. New solution is ~3x
    faster.
    
    Also threw in a change to make Skyrim's PERK available for Names and
    Text patching.

--- Mopy/bash/game/skyrim/records.py
+++ Mopy/bash/game/skyrim/records.py
@@ -3114,7 +3114,7 @@ class MreLscr(MelRecord):
     melSet = MelSet(
         MelEdid(),
         MelIcons(),
-        MelLString('DESC','description'),
+        MelLString(b'DESC', u'text'),
         MelConditions(),
         MelFid('NNAM','loadingScreenNIF'),
         MelFloat('SNAM', 'initialScale'),
@@ -4793,7 +4793,7 @@ class MreSndr(MelRecord):
         MelStruct('LNAM','sBsB',('unkSndr1',null1),'looping',
                   ('unkSndr2',null1),'rumbleSendValue',),
         MelStruct('BNAM','2b2BH','pctFrequencyShift','pctFrequencyVariance','priority',
-                  'dbVariance','staticAttenuation',),
+                  'dbVariance','staticAtten',),
     )
     __slots__ = melSet.getSlotsUsed()
 
